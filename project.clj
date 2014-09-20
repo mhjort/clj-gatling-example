e9fa4c56d6975420b8e535e4dcd3fa7dbc2c38d0
Markus Hjort <markus.hjort@reaktor.fi>
2014-09-20 18:31:41 +0300

    Bumb clj-gatling version to 0.0.6

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-gatling "0.0.5"]]
+                 [clj-gatling "0.0.6"]]
   :main clj-gatling-example.core)
