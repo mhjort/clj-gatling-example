317ed57c813162999dd0b9bbf766af58df8eb252
Markus Hjort <markus.hjort@reaktor.fi>
2014-10-29 20:03:49 +0200

    Take Clojure 1.6.0 into use

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [http-kit "2.1.16"]
                  [clj-gatling "0.1.3"]]
   :main clj-gatling-example.core)
