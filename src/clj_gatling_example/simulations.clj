f738470f1a75a1b4659bb2ed8bfe7bb8ed869c18
Markus Hjort <markus.hjort@reaktor.fi>
2015-04-14 19:15:30 +0300

    Add metrics simulation

--- src/clj_gatling_example/simulations.clj
+++ src/clj_gatling_example/simulations.clj
@@ -7,9 +7,29 @@
   (let [check-status (fn [{:keys [status]}] (callback (= 200 status)))]
     (http/get (str base-url url) {} check-status)))
 
+(defn- http-get-with-ids [url ids user-id context callback]
+  (let [check-status (fn [{:keys [status]}] (callback (= 200 status)))
+        id (nth ids user-id)]
+    (http/get (str base-url url id) {} check-status)))
+
 (def ping-simulation
   [{:name "Ping scenario"
-   :requests [{:name "Ping Endpoint" :fn (partial http-get "/ping")}]}])
+    :requests [{:name "Ping Endpoint" :fn (partial http-get "/ping")}]}])
+
+(def article-ids (cycle (range 100 300)))
+
+(def program-ids (cycle (range 200 500)))
+
+(def metrics-simulation
+  [{:name "Article read scenario"
+    :weight 2
+    :requests [{:name "Article read request"
+                :fn (partial http-get-with-ids "/metrics/article/read/" article-ids)}]}
+   {:name "Program start scenario"
+    :weight 1
+    :requests [{:name "Program start request"
+                :fn (partial http-get-with-ids "/metrics/program/start/" program-ids)}]}])
 
 (def simulations
-  {:ping ping-simulation})
+  {:ping ping-simulation
+   :metrics metrics-simulation})
