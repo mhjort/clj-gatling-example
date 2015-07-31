02f06f1abacf7a19f7e68ad0bf05b8b35c35c254
Markus Hjort <markus.hjort@reaktor.fi>
2015-07-31 11:15:01 +0300

    Take clj-gatling 0.6.1 into use

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [clj-gatling "0.6.0"]]
+                 [clj-gatling "0.6.1"]]
   :main clj-gatling-example.core)
