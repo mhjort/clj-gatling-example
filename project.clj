624c624be4f2df6ecd9588fcd69b7cabac98866d
Markus Hjort <markus.hjort@gmail.com>
2016-01-28 14:38:15 +0200

    Basic setup for Running tests in Lambda

--- project.clj
+++ project.clj
@@ -3,6 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [clj-gatling "0.7.5"]]
-  :main clj-gatling-example.core)
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :profiles {:local {:dependencies [[org.clojure/tools.nrepl "0.2.11"]
+                                    [clj-gatling "0.7.5"]]
+                     :main clj-gatling-example.core}
+             :rc {:dependencies [[org.clojure/tools.nrepl "0.2.11"]
+                                 [clojider-remote "0.1.0-SNAPSHOT"]]}
+             :lambda {:dependencies [[clojider-lambda "0.1.0-SNAPSHOT"]]}})
