b4039307037dcfc684b6c8bb2d4a3055a96732f0
Markus Hjort <markus.hjort@gmail.com>
2016-01-28 22:13:01 +0200

    More exclusions

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [clojider "0.1.0-SNAPSHOT"]]
-  :uberjar-exclusions [#"scala.*"])
+  :uberjar-exclusions [#"scala.*" #".*typesafe.*" #"io/gatling.*"])
