5355fd787a159c13ae4a46d0eb2b8838d8424588
Markus Hjort <markus.hjort@gmail.com>
2016-04-17 17:50:46 +0300

    Convert simulations to new schema

--- src/clj_gatling_example/simulations.clj
+++ src/clj_gatling_example/simulations.clj
@@ -1,23 +1,31 @@
 (ns clj-gatling-example.simulations
-  (:require [org.httpkit.client :as http]))
+  (:require [clojure.core.async :refer [go chan >!]]
+            [org.httpkit.client :as http]))
 
 (def base-url "http://clj-gatling-demo-server.herokuapp.com")
 
-(defn- http-get [url callback _]
-  (let [check-status (fn [{:keys [status]}] (callback (= 200 status)))]
-    (http/get (str base-url url) {} check-status)))
+(defn- http-get [url ctx]
+  (let [return (chan)
+        check-status (fn [{:keys [status]}]
+                       (go (>! return [(= 200 status) ctx])))]
+    (http/get (str base-url url) {} check-status)
+    return))
 
-(defn- http-get-with-ids [url ids callback {:keys [user-id]}]
-  (let [check-status (fn [{:keys [status]}] (callback (= 200 status)))
+(defn- http-get-with-ids [url ids {:keys [user-id] :as ctx}]
+  (let [return (chan)
+        check-status (fn [{:keys [status]}]
+                       (go (>! return [(= 200 status) ctx])))
         id (nth ids user-id)]
-    (http/get (str base-url url id) {} check-status)))
+    (http/get (str base-url url id) {} check-status)
+    return))
 
 (def ping
   (partial http-get "/ping"))
 
 (def ping-simulation
-  [{:name "Ping scenario"
-    :requests [{:name "Ping Endpoint" :fn ping}]}])
+  {:name "ping-simulation"
+   :scenarios [{:name "Ping scenario"
+                :steps [{:name "Ping Endpoint" :request ping}]}]})
 
 (def article-read
   (partial http-get-with-ids "/metrics/article/read/" (cycle (range 100 200))))
@@ -26,14 +34,15 @@
   (partial http-get-with-ids "/metrics/program/start/" (cycle (range 200 400))))
 
 (def metrics-simulation
-  [{:name "Article read scenario"
-    :weight 2
-    :requests [{:name "Article read request"
-                :fn article-read}]}
-   {:name "Program start scenario"
-    :weight 1
-    :requests [{:name "Program start request"
-                :fn program-start}]}])
+  {:name "metrics-simulation"
+   :scenarios [{:name "Article read scenario"
+                :weight 2
+                :steps [{:name "Article read request"
+                         :request article-read}]}
+               {:name "Program start scenario"
+                :weight 1
+                :steps [{:name "Program start request"
+                         :request program-start}]}]})
 
 (def simulations
   {:ping ping-simulation
