6b781bd83eadff2ac74c0dffde804be0df0b54a1
Markus Hjort <markus.hjort@gmail.com>
2016-04-17 16:09:55 +0300

    Upgrade to clojider 0.2.0

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [clojider "0.1.4"]]
+                 [clojider "0.2.0"]]
   :uberjar-exclusions [#"scala.*" #".*typesafe.*" #"io/gatling.*"]
-  :main clj-gatling-example.core)
+  :main clojider.core)
