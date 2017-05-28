3c620bce8d82dc52b34f9892b270e0bede53b7b1
Markus Hjort <markus.hjort@gmail.com>
2017-05-28 21:04:35 +0300

    Add example on how to use concurrency distribution function

--- src/clj_gatling_example/core.clj
+++ src/clj_gatling_example/core.clj
@@ -4,17 +4,25 @@
             [clj-time.core :as time])
   (:gen-class))
 
+(defn- ramp-up-distribution [percentage-at _]
+  (cond
+    (< percentage-at 0.1) 0.1
+    (< percentage-at 0.2) 0.2
+    :else 1.0))
+
 (defn -main [simulation users requests & [option]]
   (let [simulation (or ((keyword simulation) simulations)
                        (throw (Exception. (str "No such simulation " simulation))))]
     (if (= "--no-report" option)
       (gatling/run simulation
                  {:concurrency (read-string users)
+                  :concurrency-distribution ramp-up-distribution
                   :reporter {:writer (fn [_ _ _])
                              :generator (fn [simulation]
                                           (println "Ran" simulation "without report"))}
                   :requests (read-string requests)})
     (gatling/run simulation
                  {:concurrency (read-string users)
+                  :concurrency-distribution ramp-up-distribution
                   :root "tmp"
                   :requests (read-string requests)}))))
